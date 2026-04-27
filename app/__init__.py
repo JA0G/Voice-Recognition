d628c26b1d0cd23636a152b2a8d88c7f771d4364
Juan Andres Olmos Galvis <sanjuan1224@gmail.com>
2026-04-27 01:06:09 -0500

    Modificaciones sin finalizar

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,39 @@
+import os
+os.environ["HF_HUB_DISABLE_SYMLINKS"] = "1"
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
+
 from flask import Flask, request, jsonify
 from speechbrain.pretrained import SpeakerRecognition
-import os
 import requests
 import subprocess
 
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+AUTHORIZED_FOLDER = os.path.join(BASE_DIR, "authorized_users")
+THRESHOLD = 0.75
 
 verification = SpeakerRecognition.from_hparams(
     source="speechbrain/spkrec-ecapa-voxceleb",
     savedir="tmp_model",
     run_opts={"device": "cpu"}
 )
+
+def find_authorized_user(temp_audio_path):
+    if not os.path.exists(AUTHORIZED_FOLDER):
+        return None
+
+    for filename in os.listdir(AUTHORIZED_FOLDER):
+        if filename.endswith(".wav"):
+            user_path = os.path.join(AUTHORIZED_FOLDER, filename)
+
+            score, prediction = verification.verify_files(
+                user_path,
+                temp_audio_path
+            )
+
+            if bool(prediction) and score > THRESHOLD:
+                return filename.replace(".wav", "")
+
+    return None
 
 
 def create_app():
@@ -32,14 +56,16 @@ def create_app():
             "temp.wav"
         ], check=True)
 
-        score, prediction = verification.verify_files(
-            "authorized.wav",
-            "temp.wav"
-        )
-
-        if bool(prediction):
-            return jsonify({"resultado": "valido"})
+        user = find_authorized_user("temp.wav")
+        if user:
+            return jsonify({
+                "resultado": "valido",
+                "usuario": user
+                })
         else:
             return jsonify({"resultado": "invalido"})
-
+        
+    if not os.path.exists(AUTHORIZED_FOLDER):
+        os.makedirs(AUTHORIZED_FOLDER)
+        
     return app
