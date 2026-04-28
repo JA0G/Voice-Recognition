7bf89c32b488b0c37f0cdaef32b7790d679e1cbc
Juan Andres Olmos Galvis <sanjuan1224@gmail.com>
2026-04-28 16:36:49 -0500

    Multiusuario de varios audios

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ import subprocess
 
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 AUTHORIZED_FOLDER = os.path.join(BASE_DIR, "authorized_users")
+THRESHOLD = 0.6
 
 verification = SpeakerRecognition.from_hparams(
     source="speechbrain/spkrec-ecapa-voxceleb",
@@ -20,25 +21,44 @@ def find_authorized_user(temp_audio_path):
     if not os.path.exists(AUTHORIZED_FOLDER):
         return None
 
-    for filename in os.listdir(AUTHORIZED_FOLDER):
-        if filename.endswith(".wav"):
-            user_path = os.path.join(AUTHORIZED_FOLDER, filename)
+    best_score = 0
+    best_user = None
 
-            score, prediction = verification.verify_files(
-                user_path,
-                temp_audio_path
-            )
-            
-            print("-----")
-            print("Comparando con:", filename)
-            print("Score:", score.item())
-            print("Prediction:", prediction.item())
+    for user_name in os.listdir(AUTHORIZED_FOLDER):
+        user_folder = os.path.join(AUTHORIZED_FOLDER, user_name)
 
-            if bool(prediction):
-                return filename.replace(".wav", "")
+        if os.path.isdir(user_folder):
+
+            for filename in os.listdir(user_folder):
+                if filename.endswith(".wav"):
+                    user_path = os.path.join(user_folder, filename)
+
+                    score, prediction = verification.verify_files(
+                        user_path,
+                        temp_audio_path
+                    )
+
+                    score_value = score.item()
+
+                    print("-----")
+                    print("Usuario:", user_name)
+                    print("Archivo:", filename)
+                    print("Score:", score_value)
+
+                    # Guardar el mejor score encontrado
+                    if score_value > best_score:
+                        best_score = score_value
+                        best_user = user_name
+
+    print("====== RESULTADO FINAL ======")
+    print("Mejor usuario:", best_user)
+    print("Mejor score:", best_score)
+
+    # Aplicar threshold
+    if best_score >= THRESHOLD:
+        return best_user
 
     return None
-
 
 def create_app():
     app = Flask(__name__)
